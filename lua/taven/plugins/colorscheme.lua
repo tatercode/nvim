1198f8acec26d8fb5c73050fb33efcdceb0275e7
tatercode <Your Correct Email Here>
2025-04-30 22:07:30 -0500

    Changed colorscheme to rosepine

--- lua/taven/plugins/colorscheme.lua
+++ lua/taven/plugins/colorscheme.lua
@@ -1,11 +1,21 @@
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavor = "mocha"
+-- return { 
+--   "catppuccin/nvim", 
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     require("catppuccin").setup({
+--       flavor = "mocha"
+--     })
+--   vim.cmd("colorscheme catppuccin")
+--   end,
+-- }
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+    require("rose-pine").setup({
+      variant = "moon" -- main, moon, or dawn.
     })
-  vim.cmd("colorscheme catppuccin")
-  end,
+		vim.cmd("colorscheme rose-pine")
+	end
 }
